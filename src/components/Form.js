6d7dc2cb6dd2ac605631ed999730cadaa658534a
Hypex <zaghouane.abdallah@gmail.com>
2023-12-12 19:33:06 +0100

    added style and form save

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,64 +1,178 @@
 import React, { useState } from "react";
 import Save from "../assets/images/saveblack.png"
+import SaveFull from "../assets/images/saveblackfull.png"
+import Delete from "../assets/images/remove.png"
+import More from "../assets/images/more.png"
+
 export default function Form() {
   const [elements, setElements] = useState([]);
+  const [handleSavePicture,sethandleSavePicture] = useState(true)
+  const [formData , setFormData] = useState({
+    title : "Form",
+    fieldsLabels : {
+      textTitle : "Label Text",
+      passwordTitle : "Label Text",
+      emailTitle : "Label Text",
+      textareaTitle : "Label Text",
+      radioTitle : {
+        first : "Text",
+        second : "Text",
+        third : "Text",
+      },
+      checkboxTitle : {
+        first : "Text",
+        second : "Text",
+        third : "Text",
+        
+      },
+      dropdownTitle : "Label Text",
+      dateTitle : "Label Text"
+    }
+  })
 
   function handleDrop(e) {
     const fieldType = (e.dataTransfer && e.dataTransfer.getData("fieldType")) || "";
     setElements(prevElements => [...prevElements, fieldType]);
   }
+
+  function handleFormChange(e) {
+  const { dataset, textContent } = e.target;
+  const { name } = dataset;
+  const updateFormData = (key, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      fieldsLabels: {
+        ...prevData.fieldsLabels,
+        [key]: value,
+      },
+    }));
+  };
+  const updateRadio = (key, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      fieldsLabels: {
+        ...prevData.fieldsLabels,
+        radioTitle: {
+          ...prevData.fieldsLabels.radioTitle,
+          [key]: value,
+        },
+      },
+    }));
+  };
+  const updateChecbkox = (key, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      fieldsLabels: {
+        ...prevData.fieldsLabels,
+        checkboxTitle: {
+          ...prevData.fieldsLabels.radioTitle,
+          [key]: value,
+        },
+      },
+    }));
+  };
+  
+
+  switch (name) {
+    case "formTitle":
+      setFormData((prevData) => ({
+        ...prevData,
+        title: textContent,
+      }));
+      break;
+    case "textTitle":
+      updateFormData("textTitle", textContent);
+      break;
+    case "passwordTitle":
+      updateFormData("passwordTitle", textContent);
+      break;
+    case "emailTitle":
+      updateFormData("emailTitle", textContent);
+      break;
+    case "textareaTitle":
+      updateFormData("textareaTitle", textContent);
+      break;
+    case "radio1":
+      updateRadio("first", textContent);
+      break;
+    case "radio2":
+      updateRadio("second", textContent);
+      break;
+    case "radio3":
+      updateRadio("third", textContent);
+      break;
+    case "checkbox1":
+      updateChecbkox("first", textContent);
+      break;
+    case "checkbox2":
+      updateChecbkox("second", textContent);
+      break;
+    case "checkbox3":
+      updateChecbkox("third", textContent);
+      break;
+    case "dropdownTitle":
+      updateFormData("dropdownTitle", textContent);
+      break;
+    case "dateTitle":
+      updateFormData("dateTitle", textContent);
+      break;
+    default:
+      console.log("Something went wrong");
+  }
+}
+
   function renderInput(fieldType) {
     switch (fieldType) {
       case "Text Input":
         return (
           <div className="dropped--field" >
-            <p>Label Text</p>
+            <p onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="textTitle">{formData.fieldsLabels.textTitle}</p>
             <input type="text" placeholder="PlaceHoder" />
           </div>
         )
       case "Password Input":
         return (
           <div className="dropped--field" >
-            <p>Label Text</p>
+            <p onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="passwordTitle">{formData.fieldsLabels.passwordTitle}</p>
             <input type="password" placeholder="PlaceHoder"/>
           </div>
         )
       case "Email Input":
         return (
           <div className="dropped--field" >
-            <p>Label Text</p>
+            <p onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="emailTitle">{formData.fieldsLabels.emailTitle}</p>
             <input type="email" placeholder="PlaceHoder" />
           </div>
         )
       case "Textarea":
         return (
           <div className="dropped--field" style={{height:"100%"}}>
-            <p>Label Text</p>
+            <p onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="textareaTitle">{formData.fieldsLabels.textareaTitle}</p>
             <textarea placeholder="PlaceHoder"/>
           </div>
         )
       case "Radio Buttons":
         return (
-          <div className="dropped--field"> 
+          <div className="dropped--field" style={{height:"100%"}} > 
             <div className="radioInputs">
-              <label>Label Text1</label>
+              <label onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="radio1">{formData.fieldsLabels.radioTitle.first}</label>
               <input type="radio" />
-              <label>Label Text2</label>
+              <label onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="radio2">{formData.fieldsLabels.radioTitle.second}</label>
               <input type="radio" />
-              <label>Label Text3</label>
+              <label onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="radio3">{formData.fieldsLabels.radioTitle.third}</label>
               <input type="radio" />
             </div>
           </div>
         )
       case "Checkboxes":
         return (
-          <div className="dropped--field" >
+          <div className="dropped--field" style={{height:"100%"}} >
             <div className="radioInputs">
-              <label>Label Text1</label>
+              <label onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="checkbox1">{formData.fieldsLabels.checkboxTitle.first}</label>
               <input type="checkbox" />
-              <label>Label Text2</label>
+              <label onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="checkbox2">{formData.fieldsLabels.checkboxTitle.second}</label>
               <input type="checkbox" />
-              <label>Label Text3</label>
+              <label onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="checkbox3">{formData.fieldsLabels.checkboxTitle.third}</label>
               <input type="checkbox" />
             </div>
           </div>
@@ -66,7 +180,7 @@ export default function Form() {
       case "Dropdown Menu":
         return (
           <div className="dropped--field" >
-          <p>Label Text</p>
+          <p onBlur={handleFormChange} suppressContentEditableWarning contentEditable="true" data-name="dropdownTitle">{formData.fieldsLabels.dropdownTitle}</p>
           <select className="select--container">
             <option value="rigatoni">Rigatoni</option>
           </select>
@@ -75,7 +189,7 @@ export default function Form() {
       case "Date Input":
         return (
           <div className="dropped--field" >
-            <p>Label Text</p>
+            <p onBlur={handleFormChange} suppressContentEditableWarning  contentEditable="true" data-name="dateTitle">{formData.fieldsLabels.dateTitle}</p>
             <input type="date" />
           </div>
         )
@@ -93,6 +207,53 @@ export default function Form() {
     e.preventDefault();
   }
 
+  function SaveForm() {
+    sethandleSavePicture(prev => !prev);
+    console.log("Saved Form Data:", formData);
+  }
+
+  function deleteFormItem(index) {
+    const updatedElements = [...elements];
+    updatedElements.splice(index, 1);
+    setElements(updatedElements);
+  }
+  
+  const [stylesMenuVisible, setStylesMenuVisible] = useState(false);
+  const [stylesMenuContent, setStylesMenuContent] = useState(null);
+
+  function openMenu(element) {
+    setStylesMenuVisible(true);
+    switch (element) {
+      case "Text Input":
+      case "Password Input":
+      case "Email Input":
+      case "Textarea":
+      case "Date Input":
+        setStylesMenuContent(
+          <div className="menuItems">
+            <h1>Styles</h1>
+            {/* add styles later on */}
+            <div className="items--container">
+              <div className="items--styles">
+              </div>
+              <div className="items--styles">
+              </div>
+              <div className="items--styles">
+              </div>
+            </div>
+          </div>
+        );
+        break;
+      default:
+        setStylesMenuContent(
+          <div className="menuItems">
+            <p>No styles available for {element}</p>
+          </div>
+        );
+    }
+  }
+  
+
   return (
     <div
       className="Form--container"
@@ -101,16 +262,27 @@ export default function Form() {
     >
       <div className="form--header">
         <div>
-        <h1>Login Form</h1>
+        <h1 contentEditable="true" suppressContentEditableWarning onBlur={handleFormChange} data-name="formTitle">{formData.title}</h1>
         </div>
-        <img src={Save} alt="aa" />
+        <img src={handleSavePicture ? Save : SaveFull} alt="aa" onClick={SaveForm} />
       </div>
       {elements.map((element, index) => (
-        <div key={index}>
-          <br />
-          {renderInput(element)}
+      <div key={index} className="dropped--container">
+        <br />
+        {renderInput(element)}
+        <div>
+          <img src={Delete} alt=""onClick={() => deleteFormItem(index)} data-index={index} style={{ marginRight: element === "Submit Button" || element === "Reset Button" ? "40px" : "-130px", marginLeft: element === "Submit Button" || element === "Reset Button" ? "40px" : "-130px" }}/>
+          <br/>
+          <img src={More} alt="" onClick={() => openMenu(element)} style={{ marginRight: element === "Submit Button" || element === "Reset Button" ? "40px" : "-130px", marginLeft: element === "Submit Button" || element === "Reset Button" ? "40px" : "-130px" }}/>  
         </div>
+      </div>
       ))}
+    {stylesMenuVisible && (
+        <div className="stylesMenu">
+            {stylesMenuContent}
+          <img src={Delete} onClick={() => setStylesMenuVisible(false)} alt="" />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/LeftBar.js
+++ src/components/LeftBar.js
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Field from "./Field";
-import fieldsData from "../data/fieldsData.js"
+import fieldsData from "../data/fieldsData.js";
 
 export default function LeftBar() {
-    
-    const fieldsArray = fieldsData.map(field => {
-        return(
+    const [active, setActive] = useState(true);
+    const fieldsArray = fieldsData.map((field) => {
+        return (
             <Field
-                key = {field.id}
-                name = {field.name}
-                url = {field.url}
+                key={field.id}
+                name={field.name}
+                url={field.url}
             />
-        )
-    })
-  return (
-    <div className="leftbar--container">  
-        <div className="leftbar--headers">
-            <div className="leftbar--headers--elements" style={{marginRight:"50px"}}>
-                <p>Frequently used</p>
+        );
+    });
+
+    return (
+        <div className="leftbar--container">
+            <div className="leftbar--headers">
+                <div className="leftbar--headers--elements" style={{ marginRight: "50px" }}>
+                    <p
+                        onClick={() => setActive(true)}
+                        style={{
+                            borderBottom: active ? "1px solid #24a68a" : "none",
+                        }}
+                    >
+                        Frequently used
+                    </p>
+                </div>
+                <p>|</p>
+                <div className="leftbar--headers--elements" style={{ marginLeft: "50px" }}>
+                <p
+                        onClick={() => setActive(false)}
+                        style={{
+                            borderBottom: !active ? "1px solid #24a68a" : "none",
+                        }}
+                    >
+                        More Fields
+                    </p>
+                </div>
             </div>
-            <p>|</p>
-            <div className="leftbar--headers--elements" style={{marginLeft:"50px"}}>
-                <p>More Fields</p>
-            </div>
+            <div className="fields--container">{fieldsArray}</div>
         </div>
-        <div className="fields--container">
-            {fieldsArray}
-        </div>
-    </div>
-  );
+    );
 }
